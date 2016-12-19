c9f2c7e7afed942b8801e7d18dabfbdfbb608e89
MREIGHT\mreight <muhammad14@mhs.if.its.ac.id>
2016-12-19 18:32:55 +0700

    init client, tak bentuk class biar reusable sama bisa get attribut, gak perlu if if an command

---
+++ client/client.py
@@ -0,0 +1,54 @@
+import socket
+import os
+from select import select
+
+
+class Client:
+    def __init__(self, (ip, port)):
+        self.serverIp = ip
+        self.port = port
+        self.server = None
+        self.data_sock	= None
+        self.recvData = ""
+        self.currentDirectory = currentDirectory
+        self.isi = ""
+
+    def run(self):
+        self.open_socket()
+        while True:
+			# input command
+            command = raw_input('Command: ')
+            if not command:
+                break
+            else:
+                print '---- Command:', command.split()[0].strip().upper()
+                try:
+                    function = getattr(self, command.split()[0].strip().upper())
+                    print "masuk getattr"
+                    command+='\r\n'
+                    print "masuk getattr 2"
+                    function(command)
+                except Exception, e:
+                    print 'ERROR:', e
+					
+	def open_socket(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((self.serverIp, self.port))
+        # msg = self.server.recv(2048)
+        # print msg
+        while True:
+            reads, writes, errors = select([self.server], [], [], 1)
+            if not reads:
+                break
+            else:
+                for read in reads:
+                    msg = self.server.recv(2048)
+                    print msg.strip()
+
+def main():
+    new_client = Client(('localhost',300000))
+    # new_client = Client(('10.151.43.17', 12345))
+    new_client.run()
+
+if __name__ == '__main__':
+    main()
