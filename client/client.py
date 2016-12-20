5978b3fba908a92ed4acb185769c8acaa9ef59f1
MREIGHT\mreight <muhammad14@mhs.if.its.ac.id>
2016-12-20 10:24:39 +0700

    list bener rek, salah ngeprint tok

--- client/client.py
+++ client/client.py
@@ -78,16 +78,18 @@ class Client:
         print msg.rstrip()
 
     def LIST(self, command):
+
         port = self.PASV("PASV\r\n")
+
+        self.server.send(command)
+        msg = self.server.recv(BUFFER)
+        print msg.strip()
 
         self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.data_sock.connect((self.serverIp, port))
-        self.server.send(command)
 
         self.recvdata = self.data_sock.recv(1024)
         print self.recvdata.strip()
-        msg = self.server.recv(1024)
-        print msg.strip()
         msg = self.server.recv(1024)
         print msg.strip()
 
